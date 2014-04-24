147b99568d0c77e914ec0cd4de85a7b1f04d219d
Emerson Estrella <emerson.estrella@gmail.com>
2014-04-24 08:03:07 +0200

    ...

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var twit = new twitter({
 });
 
 var settings = {
-  app_port: 8888,
-  websocket_port: 8080,
+  app_port: 7777,
+  websocket_port: 80,
   scoring: {
     characters: 1,
     retweet: 10,
@@ -21,50 +21,91 @@ var settings = {
     hashtag: 4,
     symbols: 2,
     urls: 3,
+    coordinates: 4,
     user_mentions: 5,
     media: 8
-  },
-  p1Hash: null,
-  p2Hash: null
+  }
 };
 
 var clients = [];
-
-function handler(req, res) {
-  fs.readFile(__dirname + '/public/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
-}
 
 app.listen(settings.app_port);
 
 io.sockets.on('connection', function (socket) {
   console.log("clients: "+clients.length);
   if(clients.length < 2){
+
+    socket.set('points', 0);
     clients.push(socket);
 
     console.log("connected: "+socket.id);
     console.log("clients: "+clients.length);
 
-    //clients[socket.id] = socket;
-
     socket.on('changeHash', function(data){
       console.log("changeHash");
       console.log(data);
       socket.set('hash', data.newHash);
-      changeHashes();
+
+      clients[0].get('hash', function (err, p1Hash) {
+        if(p1Hash!=null){
+          clients[1].get('hash', function (err, p2Hash) {
+            if(p2Hash!=null){
+              //io.sockets.emit('racing');
+              console.log('racing: #'+p1Hash+' vs #'+p2Hash);
+
+              twit.stream('statuses/filter', { language: 'en', track: p1Hash+','+p2Hash}, function(stream) {            
+                stream.on('data', function(data) {
+                  if((data.text !== undefined)&&(data.entities.hashtags.length > 0)) {
+                    c = null;
+                    for (key in data.entities.hashtags) {
+                      if(data.entities.hashtags[key].text.toLowerCase() == p1Hash.toLowerCase()){
+                        clients[0].get('points', function (err, p) {
+                          points = p + (data.text.length * settings.scoring.characters);
+                          /*
+                          //retweets
+                          points += data.retweet_count * settings.scoring.retweet;
+                          //favorites
+                          points += data.favorite_count * settings.scoring.favorite;
+                          //hashtags
+                          points += data.entities.hashtags.length * settings.scoring.hashtag;
+                          //symbols
+                          points += data.entities.symbols.length * settings.scoring.symbols;
+                          //urls
+                          points += data.entities.urls.length * settings.scoring.urls;
+                          //user_mentions
+                          points += data.entities.user_mentions.length * settings.scoring.user_mentions;
+                          //media
+                          if(data.entities.media !== undefined)
+                            points += data.entities.media.length * settings.scoring.media;
+                          */
+                          clients[0].set('points', points);
+                          console.log(p1Hash+': '+points);
+                        });
+                      }
+                      else if(data.entities.hashtags[key].text.toLowerCase() == p2Hash.toLowerCase()){
+                        clients[1].get('points', function (err, p) {
+                          points = p + (data.text.length * settings.scoring.characters);
+                          clients[1].set('points', points);
+                          console.log(p2Hash+': '+points);
+                        });
+                      }
+                    }
+                  }
+                });
+                //setTimeout(stream.destroy, 15000);
+              });
+              
+            }
+          });
+        }
+      });
+
     });
 
     if(clients.length < 2){
       socket.emit('waiting');
     }
-    else if(clients.length == 2){
+    else{
       io.sockets.emit('go');
 
       twit.stream('statuses/sample', {
@@ -79,9 +120,9 @@ io.sockets.on('connection', function (socket) {
             }
           }
         });
-        setTimeout(stream.destroy, 15000);
+        setTimeout(stream.destroy, 10000);
       });
-
+     
     }
 
     socket.on('disconnect', function(){
@@ -96,29 +137,17 @@ io.sockets.on('connection', function (socket) {
       io.sockets.emit('waiting');
     });
 
-  }else{
-    console.log("2 players only demo");
   }
 });
 
-function changeHashes(){
-  clients[0].get('hash', function (err, p1Hash) {
-    if(p1Hash!=null){
-      clients[1].get('hash', function (err, p2Hash) {
-        if(p2Hash!=null){
-          io.sockets.emit('racing');
-
-          console.log(p1Hash+','+p2Hash);
-
-          twit.stream('statuses/filter', { language: 'en', track: p1Hash+','+p2Hash}, function(stream) {
-              stream.on('data', function(data) {
-                io.sockets.emit('tweet', {data: data});
-              });
-              setTimeout(stream.destroy, 15000);
-          });
-
-        }
-      });
+function handler(req, res) {
+  fs.readFile(__dirname + '/public/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
     }
+    res.writeHead(200);
+    res.end(data);
   });
 }
