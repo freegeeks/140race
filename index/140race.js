9d28a99f0f03f0b8c2d3207b74620707c07e6738
Renan Gonçalves <renan.saddam@gmail.com>
2014-04-24 11:54:00 +0200

    Smooth track transition.

--- index/140race.js
+++ index/140race.js
@@ -1,7 +1,5 @@
 $(document).keyup(function(e) {
-      if (e.keyCode == 13) {  // enter
-
-      //moveTrack("1900", 6000)
+    if (e.keyCode == 13) {  // enter
 
         $( ".player_1" ).removeClass("p1_anim").addClass("winner");
         $( ".player_2" ).removeClass("p2_anim").addClass("loser");
@@ -10,44 +8,21 @@ $(document).keyup(function(e) {
         
         setInterval(function() {
             $( ".div_winner" ).delay(500).fadeIn(800);
-        }, 300);      
-
-      moveTrack("0", 60000)
-
-
-
-      } 
+        }, 300);
+    } 
 });
 
 
+function moveTrack() {
+    var track = $('.track');
+    var bottom = track.parent().height() - track.height();
 
-function moveTrack(position, speedy) {
-
-    $( ".track" ).animate({
-        bottom: "-="+position+"px"
-    }, speedy, "linear", function() {
-    
-    var p = $( ".track" );
-    var position = p.position();
-    var topPos = position.top
-    console.log(topPos)
-
-    if ( topPos >= "-140" ) {
-        $( ".track" ).css("bottom", "0%")
-    }
-
-  });
-
+    track.css('bottom', '-30px');
+    track.animate({bottom: bottom}, 6000, 'linear', function() {
+      moveTrack();
+    });
 };
-
-
-
-
-setInterval(function() {
-    moveTrack("1400", 6000)
-}, 6000);
-
-
+moveTrack();
 
 
 $( ".login-bt" ).on( "click", function() {
@@ -55,26 +30,15 @@ $( ".login-bt" ).on( "click", function() {
 
     $( ".player_1" ).addClass("p1_anim");
     $( ".player_2" ).addClass("p2_anim");
-    
-    moveTrack("1400", 6000);
-
 });
-
-
-
-
-$(".tap").on('touchstart mousedown',function(e){
-    moveTrack("250")
-})
 
 
 
 // to be removed
 var number = 1 + Math.floor(Math.random() * 60);
 setInterval(function() {
-  var number = 1 + Math.floor(Math.random() * 60);
-  var n2 = number + 8
-  $('.p1').text(number);
-  $('.p2').text(n2);
-},
-400);
+    var number = 1 + Math.floor(Math.random() * 60);
+    var n2 = number + 8
+    $('.p1').text(number);
+    $('.p2').text(n2);
+}, 400);
